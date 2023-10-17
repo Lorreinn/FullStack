2cbfee6d3ece853dc7549984f74fc2b4a47aacc3
Esther <estherrz2996@gmail.com>
2023-10-17 14:51:23 +0200

    resenas

---
+++ ProyectoJs/04-arrays.js
@@ -0,0 +1,11 @@
+//EJERCICIO DE OBJETOS
+
+let variableA = "valora";
+    variableB = "valorb";
+    variableC = "valorc";
+ 
+let ejercicio6 = {};
+
+ejercicio6.variable1 = variableA;
+ejercicio6.variable2 = variableB;
+ejercicio6.varuable3 = variableC;
